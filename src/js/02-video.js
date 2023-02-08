b7354d8bb8f04a36c531f589dff43333b5d6cff6
Zabaraaa <112432785+Zabaraaa@users.noreply.github.com>
2023-02-08 02:52:26 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const videoplayerCurrentTime = 'videoplayer-current-time';
+
+
+player.on('timeupdate', throttle(onSaveStorage, 1000));
+
+function onSaveStorage(event) {
+    localStorage.setItem(videoplayerCurrentTime, event.seconds);
+
+    if (event.seconds === event.duration) {
+    
+        localStorage.removeItem(videoplayerCurrentTime);
+    }
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+    const saveTime = localStorage.getItem(videoplayerCurrentTime);
+    if (saveTime) {
+    player.setCurrentTime(saveTime);
+    }
+}
